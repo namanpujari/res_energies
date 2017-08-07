18b3ad20fdc393c39333f7f7fedec876dca736ca
Naman Pujari <namanpujari913@gmail.com>
2017-08-07 15:19:11 -0400

    Major changes + condenser COMPLETE

--- related_programs/fort36_condenser.py
+++ related_programs/fort36_condenser.py
@@ -16,17 +16,23 @@ def fort36Ave_condensed(fort36_file, out_location):
 	# to combine multiple statistics. For example, we can get values for 
 	# both mean and std, grouped by pH, in one line.
 	_res = AverE_df.groupby(['pH'], sort=False).agg( ['mean', 'std'] )
+	_res.columns = ['Mean', 'Std']
+	
+	for type in ['Mean', 'Std']:
+		_res[type] = ['%.3f' % round(val,3) for val in 
+			_res[type]]
+
 	file_location = out_location
 	if not os.path.exists(file_location):
 		os.makedirs(file_location)
 	
-	file_name = 'fort.36_condensed.csv'
+	file_name = 'fort98'
 	
 	to_write = os.path.join(file_location, file_name)
 	_res.to_csv(to_write, sep = ' ')
 
 
-def fort36Conformers_std(fort36_file):#, out_location):
+def fort36Conformers_std(fort36_file, out_location):
 	#getcontext().prec = 2
 	master_df = pd.DataFrame()
 
@@ -54,10 +60,46 @@ def fort36Conformers_std(fort36_file):#, out_location):
 
 	master_df.index.name = 'pH'	
 	
-	transposed = master_df.transpose()
-	master_df.transpose().to_csv('fort99', sep = ' ', header = ['pH            0.00  1.00  \
+	#transposed = master_df.transpose()
+	
+	file_location = out_location
+	if not os.path.exists(file_location):
+		os.makedirs(file_location)
+	file_name = 'fort99'
+	to_write_loc = os.path.join(file_location, file_name)
+
+	master_df.transpose().to_csv(to_write_loc, sep = ' ', header = ['pH            0.00  1.00  \
 2.00  3.00  4.00  5.00  6.00  7.00  8.00  9.00 10.00 11.00 12.00 13.00 14.00','', '', '',
 		'', '', '', '', '', '', '','' , '', '', ''])
+	
+	std_cap = 0.100
+	high_std = []
+	analysis_file = open(to_write_loc, 'r').readlines()
+	for line_index in range(len(analysis_file)):
+		if(line_index == 0): pass
+		else:
+			for word in analysis_file[line_index].split():
+				try:
+					compare = float(word)
+					if(compare >= std_cap):
+						high_std.append(line_index)
+				except ValueError:
+					print(word +  'is not a float')
+
+	with open(to_write_loc, 'a') as append:
+		append.write('''-------------------------------------------------------------------------------------------------
+------------------------------------------SUMMARY------------------------------------------------
+-------------------------------------HIGH STD CONFORMERS-----------------------------------------
+
+''')
+		if(len(high_std) == 0):
+			summary = 'NO HIGH STD CONFORMERS FOUND'
+		else:
+			summary = ''.join([analysis_file[line] for
+						line in high_std])
+		append.write(summary)
+		append.close()	
+
 
 def ask_arguments():
 	parser = ArgumentParser(description = '''Analyze values in fort36, 
@@ -84,7 +126,7 @@ def ask_arguments():
 if __name__ == '__main__':
 	args = ask_arguments()
 	fort36Ave_condensed(args.fort36_location, args.output_location)
-	fort36Conformers_std(args.fort36_location)
+	fort36Conformers_std(args.fort36_location, args.output_location)
 
 
 
